6a800ac257b90441a54f2ef25bf5503d8167c085
Ranjan Verma <verma.ranjan62@gmail.com>
2022-08-05 21:03:56 +0530

    Dynamic Values in React

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 function App() {
+  const title = 'Blog Post';
+  const body = 'This is my blog post';
+  const comments = [
+    {
+      id: 1,
+      text: 'Comment One',
+    },
+    { id: 2, text: 'Comment Two' },
+    { id: 3, text: 'Comment Three' },
+  ];
   return (
     <div className="container">
-      <h1>My App</h1>
+      <h1>{title.toUpperCase()} </h1>
+      <p>{body}</p>
+
+          <div className="comments">
+              <h3>
+                Comments ({comments.length})
+              </h3>
+              <ul>
+                  {comments.map((comment, index) => {
+                      return (
+                          <li key={index}>{comment.text}</li>
+                      )
+                  })}
+              </ul>
+   </div>
     </div>
     /**
      * return React.createElement('div', {className: "container"}, React.createElement("h1", {}, 'My App'))
