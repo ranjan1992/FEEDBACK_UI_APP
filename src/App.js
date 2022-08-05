54b6794a6c9a9b699ec1656e1f3035cd6b3ebd8f
Ranjan Verma <verma.ranjan62@gmail.com>
2022-08-05 21:13:43 +0530

    Conditional in JSX

--- src/App.js
+++ src/App.js
@@ -9,23 +9,25 @@ function App() {
     { id: 2, text: 'Comment Two' },
     { id: 3, text: 'Comment Three' },
   ];
+
+  const loading = false;
+  const showComments = true;
+
+  if (loading) return <h1>Loading ....</h1>;
   return (
     <div className="container">
       <h1>{title.toUpperCase()} </h1>
       <p>{body}</p>
-
-          <div className="comments">
-              <h3>
-                Comments ({comments.length})
-              </h3>
-              <ul>
-                  {comments.map((comment, index) => {
-                      return (
-                          <li key={index}>{comment.text}</li>
-                      )
-                  })}
-              </ul>
-   </div>
+      {showComments && (
+        <div className="comments">
+          <h3>Comments ({comments.length})</h3>
+          <ul>
+            {comments.map((comment, index) => {
+              return <li key={index}>{comment.text}</li>;
+            })}
+          </ul>
+        </div>
+      )}
     </div>
     /**
      * return React.createElement('div', {className: "container"}, React.createElement("h1", {}, 'My App'))
